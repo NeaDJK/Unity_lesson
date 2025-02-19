ea01bb5b6aeaad55a3ab531c4e181c4df52ccd3c
NeaD <nikitauramne@gmail.com>
2025-02-19 23:13:15 +0300

    Добавьте файлы проекта.

---
+++ Operator_/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Operator_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 16;
+            Console.Write("Введите ваш рост: ");
+            float.TryParse(Console.ReadLine(), out float height);
+
+            Console.Write("Введите ваш вес: ");
+            float.TryParse(Console.ReadLine(), out float weight);
+
+            float BMI = weight / (height * height);
+
+            Console.WriteLine(BMI);
+        }
+    }
+}
